768742e778615e1f57333372ab77713f7e09d017
matt <matt_rigel21@hotmail.com>
2019-02-23 10:40:06 -0800

    asdf

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,6 +3,27 @@ import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 
+
+function RenderDish(props) {
+
+    const dish = props.dish;
+    
+    if (dish != null) {
+        return(
+            <Card
+                featuredTitle={dish.name}
+                image={require('./images/vadonut.png')}>
+                <Text style={{margin: 10}}>
+                    {dish.description}
+                </Text>
+            </Card>
+        );
+    }
+    else {
+        return(<View></View>);
+    }
+}
+
 class DishDetail extends Component {
 
     constructor(props) {
